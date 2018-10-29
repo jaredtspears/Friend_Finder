d4401778858e0de0ac5df0b7841fa53994d436c0
Ha1f_Deaf <jaredtspears@gmail.com>
2018-10-28 21:56:28 -0400

    attempted to update the apiRoutes.js, need to debug further but the routes are working and this algorithm seems to be what was commonly used to solve the need.

--- routing/apiRoutes.js
+++ routing/apiRoutes.js
@@ -12,59 +12,45 @@ module.exports = function(app) {
 //posting routes 
 app.post("/api/survey", function(req, res) {
 
-    // user data
-    console.log(req.body);
+    //capturing data 
+    let data = req.body; 
+    // capturing score
+    let scores = data.scores; 
+    //setting this var to descend from friends data
+    let index = -1; 
+    //lowest score 
+    let lowestScore=100;
 
-   //need to convert input data from user on survey.html to an [], so it needs to tie into the submit id maybe with jquery or with another identifier that can be called on this page.
-
-    //users score is blank array 
+    //users score is blank array NOT SURE IF I NEED THIS BELOW PART GIVEN MY ABOVE VARIABLES
     const currentUserResults = req.body.scores;
   console.log("this is current user results" + currentUserResults);
-
-//     //attempted to make a variable related to friendsLoop that calls only the name and the score from the above object. This will be so that I can compare them below
- var 
-
+ 
+//==========the loops======================
  //2 loops one over friends.js data, the other is internal and it will go over the scores and find the value
 
+ for (var i =0; i<friends.length; i++){
+     let friend = friends[i];
+     let friendsScores = friend.scores;
+     let sum = 0;
 
-
-
- 
-//     //created friendsLoop 
-    // function friendsLoop(){
-    // for(i=0;i<friends.length; i++){
-    // friends.name && friends.score;
-    // console.log(friends.name + friends.score);
-    // }
-    // }
-//     console.log("this is prior user results" + priorUsersResults);
-
-//     //trying to loop through users results then comparing it to the prior user/friends
-//     for (i=0; i<currentUserResults.length; i++){
-//     //find difference using math.abs to ensure possitive number will be returned.
-//     if (currentUserResults == priorUsersResults){
-//     function totalDifference(currentUserResults, priorUsersResults){
-//         //setting variable for logging the results
-        // let difference =  math.abs(currentUserResults - friendsData);
-        //     // res.send(lowest friend w)
-        // console.log("this is the total difference: " + difference);
-    
-//         //call function
-//         totalDifference();
-//     } else {
-//     console.log("no matches compatable at this time change yourself and see if you will find a fit");
-//     }
-//     }
-    
-  });
-
+     //nested loop 
+     for( var j = 0; j< friendsScores.length; j++){
+         let currnetFriendScore = friendScores[j];
+         let currentUserScore = scores[j];
+         let differnece = parseInt(currnetFriendScore) - parseInt(currentUserScore);
+         var absDifference = Math.abs(differnece);
+         //adding up sum of the difference
+         sum += absDifference;
+     }
+     if(sum < lowestScore) {
+         lowestScore = sum;
+         index = i;
+     }
+ }
+   //loop through current user results (score)
+//    for (var i=0; i<currentUserResults.scores.length; i++) {
+//        currentUserResults.scores[i] = parseInt(user.scores[i]);
+//    }
+   res.json(friends[index])
+});
 }
-//   //not sure if I needed this but I included it it anyway
-//   app.post("/api/clear", function(req, res) {
-//     // Empty out the friends arrays of data
-//     friendsData.length = [];
-
-//     res.json({ ok: true });
-//   });
-// };
-
